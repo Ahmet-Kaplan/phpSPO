aa10def51230fc96b6b76e37f3a51a494cee9bd1
Ahmet Kaplan <kaplan38@gmail.com>
2024-10-24 23:56:07 +0300

    Update SamlTokenProvider.php

--- src/Runtime/Auth/SamlTokenProvider.php
+++ src/Runtime/Auth/SamlTokenProvider.php
@@ -143,25 +143,35 @@ class SamlTokenProvider extends BaseTokenProvider
     protected function acquireSecurityTokenFromFederatedSTS($username, $password) {
 
         $response = Requests::get(str_replace('{username}', $username, self::$RealmUrlTemplate),null);
-        $federatedStsUrl = $this->getFederatedAuthenticationInformation($response->getContent());
+        if (!($urlNode = $this->getFederatedAuthenticationInformation($response->getContent()))) {
+            return null;
+        }
 
-        if ($federatedStsUrl) {
-          $message_id = md5(uniqid($username . '-' . time() . '-' . rand() , true));
-          $data = $this->prepareSecurityFederatedTokenRequest($username, $password, $message_id, $federatedStsUrl->textContent);
+        $urlParts = parse_url($urlNode->textContent);
+        if (!$urlParts || !isset($urlParts['host'])) {
+            return null;
+        }
 
-          $headers = array();
-          $headers['Content-Type'] = 'application/soap+xml';
-          $response = Requests::post($federatedStsUrl->textContent, $headers, $data);
+        if (!isset($urlParts['scheme'])) {
+            $urlParts['scheme'] = 'https';
+        }
 
-          $samlAssertion = $this->getSamlAssertion($response->getContent());
+        $federatedStsUrl = "{$urlParts['scheme']}://{$urlParts['host']}/adfs/services/trust/2005/usernamemixed/";
+        $message_id = md5(uniqid($username . '-' . time() . '-' . rand() , true));
+        $data = $this->prepareSecurityFederatedTokenRequest($username, $password, $message_id, $federatedStsUrl);
 
-          if ($samlAssertion) {
-            $samlAssertion_node = $samlAssertion->item(0);
-            $data = $this->prepareRST2Request($samlAssertion_node);
-            $response = Requests::post(self::$RST2Url, $headers, $data);
-            $this->useFederatedSTS = TRUE;
-            return $response;
-          }
+        $headers = array();
+        $headers['Content-Type'] = 'application/soap+xml';
+        $response = Requests::post($federatedStsUrl, $headers, $data);
+
+        $samlAssertion = $this->getSamlAssertion($response->getContent());
+
+        if ($samlAssertion) {
+          $samlAssertion_node = $samlAssertion->item(0);
+          $data = $this->prepareRST2Request($samlAssertion_node);
+          $response = Requests::post(self::$RST2Url, $headers, $data);
+          $this->useFederatedSTS = TRUE;
+          return $response;
         }
         return null;
     }
@@ -195,8 +205,8 @@ class SamlTokenProvider extends BaseTokenProvider
             $xml = new DOMDocument();
             $xml->loadXML($response);
             $xpath = new DOMXPath($xml);
-            if ($xpath->query("//STSAuthURL")->length > 0) {
-                return $xpath->query("//STSAuthURL")->item(0);
+            if ($xpath->query("//AuthURL")->length > 0) {
+                return $xpath->query("//AuthURL")->item(0);
             }
         }
         return null;
